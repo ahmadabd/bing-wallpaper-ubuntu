2f9b24c7042e8c4b9e91f776b53f5107d74f25fc
ahmadabd <ahmadabd13741112@gmail.com>
2017-01-28 14:32:47 +0330

    remove python file and added it to shell script file

--- execIndex.sh
+++ execIndex.sh
@@ -1,13 +1,43 @@
 #!/bin/bash
 
+# bing wallpaper.
 # This script is written by Ahmad Abdollahzade . use it , make it better and share it .
+# auther : Ahmad Abdollahzade (ahmadabd13741112@gmail.com)
 
 # export DBUS_SESSION_BUS_ADDRESS environment variable useful when the script is set as a cron job
 PID=$(pgrep gnome-session)
 export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ|cut -d= -f2-) 
 
-# Get download link and store it on downLink .
-downLink=$(./index.py)
+# Get download link and store it on downLink variable.
+function foundDownLink()
+{
+python - << CODE
+import re
+import requests
+import BeautifulSoup
+import os
+
+def getLink():
+    if os.uname()[0] == "Linux":
+        baseUrl = "http://www.bing.com"          
+        imgUrl = "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=1&n=1&mkt=en-US"
+        try:
+            html = requests.get(imgUrl)  # Get xml code .
+        except:
+            print ("net is off")    # it runs if net is off .
+        else:
+            soup = BeautifulSoup.BeautifulSoup(html.content)     
+            link = soup.find('url',text = re.compile('(.jpg)$'))    #Get first <url> ... </url> string .
+            downLink = baseUrl + link   # make download link .
+            print (downLink)     
+    else:
+        print ("Your os should be Linux.")
+        exit()
+getLink()
+CODE
+}
+
+downLink=$(foundDownLink)
 
 # Making pic name .
 picName=$(echo $downLink | cut -f7 -d"/")
